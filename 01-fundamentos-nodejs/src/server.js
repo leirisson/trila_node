49001412865419977ebf96f81acbb4c6507484a2
leirisson <103783610+leirisson@users.noreply.github.com>
2025-01-14 03:00:59 -0400

    subir

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,10 +1,59 @@
 import http from 'node:http'
 
+// - criar usuarios 
+// - listar usuarios
+// - edução de usuarios
+// - remoção de usuarios
 
-const server = http.createServer((req, res)=>{
-  res.end("hello word")
+// HTTP
+// - método HTTP
+// - URL
+
+// GET, POST, DELETE, PUT, PATCH
+
+// GET => buscar um recurso no back-end
+// POST => criar um recurso no back-end
+// PUT => Atualizar um recurso noback-end
+// PATCH => atualizar informações especificas de um recurso no back-end
+// DELETE => deletar um recursono back-end
+
+// GET /users => buscando usuarios no back-end
+// post /isers => criandoum usuario no back-end
+
+// TIPO DE APLICAÇÃO 
+// STATEFUL => GUARDA AS INFORMAÇÕES DA APLICAÇÃO EM MEMORIA 
+// STATELESS => GUARDA AS INFORMAÇÕES DA APLICAÇÃO EM BANCO DE DADOS
+
+// cabeçalhos (Requisição/Resposta) => Metadados
+
+// HTTP status code
+
+
+
+const users = []
+
+const server = http.createServer((req, res) => {
+  const {method, url} = req
+
+  if (method === 'GET' && url ==='/users'){
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users))
+  }
+
+  if(method === 'POST' && url === '/users'){
+   
+    users.push({
+      id:1,
+      name: 'jhon Doe',
+      email:'jhondoe@exemple.com'
+    })
+    return res.writeHead(201).end()
+  }
+
+    res.writeHead(404).end()
 })
 
-server.listen(3333, ()=>{
+server.listen(3333, () => {
   console.log('http://localhost:3333/')
 })
---
+++ 02-stremDeLeitura/src/fundamentals.js
@@ -0,0 +1,32 @@
+// Stream -> 
+
+// stdin = strem de entrada 
+// stdout = sream de saida
+// usando o pipe para ler os dados aos poucos
+// process.stdin.pipe(process.stdout)
+
+
+import { Readable } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+
+  index = 1
+  // criando uma stream de leitura do zera
+  // pode ser interpretado como leru dado do front-end 
+  // metodo obrigatorio 
+
+  _read() {
+    const i = this.index++
+    
+      setTimeout(() => {
+        if (i > 100) {
+          this.push(null)
+        } else {
+          this.push(Buffer.from(String(i)))
+        }
+      }, 1000)
+  }
+}
+
+new OneToHundredStream()
+.pipe(process.stdout)
