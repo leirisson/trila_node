c895ddf84169d4ce535e3b4c9ff096ce6419a4ad
leirisson <103783610+leirisson@users.noreply.github.com>
2025-01-27 09:49:31 -0400

    criando a tabela de transações

--- 010-modulo2/06-criando-tabela-de-transacoes/src/server.ts
+++ 010-modulo2/06-criando-tabela-de-transacoes/src/server.ts
@@ -1,16 +1,19 @@
 import fastify from 'fastify';
+import { knex } from './database'
 
 const app = fastify()
 const PORT = 3334
+
 app.get('/api/v1/transacoes', async () => {
-  return 'Começando com fastify'
+  const tabelas = await knex('sqlite_schema').select()
+  return tabelas
 })
 
 
 app.listen({
   port: PORT
 })
-.then(() => {
-  console.log('API na rota:')
-  console.log(`http://localhost:${PORT}/api/v1/transacoes`)
-})
+  .then(() => {
+    console.log('API na rota:')
+    console.log(`http://localhost:${PORT}/api/v1/transacoes`)
+  })
