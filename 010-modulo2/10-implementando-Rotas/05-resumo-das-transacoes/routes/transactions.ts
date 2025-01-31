697091755b9ab533bf67088e6bf6a5de1f797247
leirisson <103783610+leirisson@users.noreply.github.com>
2025-01-31 09:08:44 -0400

    finalizando o resumo de transações

--- 010-modulo2/10-implementando-Rotas/05-resumo-das-transacoes/routes/transactions.ts
+++ 010-modulo2/10-implementando-Rotas/05-resumo-das-transacoes/routes/transactions.ts
@@ -1,11 +1,45 @@
-import { FastifyInstance } from "fastify";
-import { knex } from "../src/database";
+import { FastifyInstance } from "fastify"
+import { knex } from "../src/database"
+import { z } from 'zod'
+import { randomUUID } from "node:crypto"
 
 export function transactionsRoutes(app: FastifyInstance) {
+
+    //resgatando todas as transações
     app.get('/', async () => {
+        const transactions = await knex('transactions').select()
 
-        const tables = await knex('sqlite_schema').select()
+        return transactions
+    })
 
-        return tables
+    // resumindo todas as transações
+    app.get('/resumo', async () => {
+
+        const resumo = await knex('transactions').sum('amount', { as: 'resumo das transações' }).first()
+
+        return { resumo }
+    })
+
+    app.post('/', async (request, reply) => {
+        //criando um schema de transação com zod 
+        // todas as requisições vão obedecer esse schema
+        const createSchemaTransactionsBodySchema = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['credit', 'debit'])
+        })
+
+        // desestruturando o body da resquest
+        const { title, amount, type } = createSchemaTransactionsBodySchema.parse(request.body)
+
+        //inserindo no banco de dados
+        await knex('transactions')
+            .insert({
+                id: randomUUID(),
+                title,
+                amount: type === 'credit' ? amount : amount * -1
+            })
+
+        return reply.status(201).send()
     })
 }
