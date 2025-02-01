5e32b26459fa51a283681d691575454cf5b9b9d6
leirisson <103783610+leirisson@users.noreply.github.com>
2025-01-31 23:47:48 -0400

    wed

--- 010-modulo2/10-implementando-Rotas/07-validando-os-cookies/routes/transactions.ts
+++ 010-modulo2/10-implementando-Rotas/07-validando-os-cookies/routes/transactions.ts
@@ -5,7 +5,6 @@ import { kenx } from "../src/database";
 export function transactionsRoutes(app: FastifyInstance){
     
     app.get('/', async () => {
-    
         const tables = kenx('sqlite_schema').select()
         return tables
     })
