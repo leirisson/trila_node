5b5193f26c3f72d23867f148e15b31429783894f
leirisson <103783610+leirisson@users.noreply.github.com>
2025-02-05 16:57:59 -0400

    subir

---
+++ 010-modulo2/10-implementando-Rotas/07-validando-os-cookies/test/example.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest'
+
+test('o usuario consegue criar uma nova transação', () => {
+
+
+    const respostaSatusCode = 201
+
+    expect(respostaSatusCode).toEqual(201)
+})
