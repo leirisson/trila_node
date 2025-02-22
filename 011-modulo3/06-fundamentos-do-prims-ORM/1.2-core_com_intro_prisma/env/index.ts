9aacb216f248d8ef9207c7916a9eefcce2bc7b9d
leirisson <leirissonsouza99@gmail.com>
2025-02-22 15:55:27 -0400

    criando projeto do zero e configurando a primeira migrate

---
+++ 011-modulo3/06-fundamentos-do-prims-ORM/1.2-core_com_intro_prisma/env/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import {z} from 'zod'
+
+
+const createEnvSchema = z.object({
+    NODE_ENV : z.enum(['dev','test','production']).default('dev'),
+    PORT : z.coerce.number().default(3334)
+})
+
+const prev_env = createEnvSchema.safeParse(process.env)
+
+if(prev_env.success === false){
+    console.error('Erro nas variaveis de ambiente. ', prev_env.error.format())
+    throw new Error('Erro nas variaveis de ambeinte. ')
+}
+
+export const env = prev_env.data 
