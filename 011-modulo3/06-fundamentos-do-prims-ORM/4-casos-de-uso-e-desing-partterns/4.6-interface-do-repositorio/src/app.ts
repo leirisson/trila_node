b9b5aca36e48ca9bcdf71197a88087f2202c31a5
leirisson <leirissonsouza99@gmail.com>
2025-03-05 17:59:36 -0400

    inserindo usuario no banco de dados

--- 011-modulo3/06-fundamentos-do-prims-ORM/4-casos-de-uso-e-desing-partterns/4.6-interface-do-repositorio/src/app.ts
+++ 011-modulo3/06-fundamentos-do-prims-ORM/4-casos-de-uso-e-desing-partterns/4.6-interface-do-repositorio/src/app.ts
@@ -1,3 +1,22 @@
 import fastify from "fastify";
+import { z } from 'zod'
 
 export const app = fastify()
+
+
+app.post('/users', async (request, reply) => {
+
+    const createBodyRequestSchema = z.object({
+        name: z.string(),
+        email: z.string(),
+        password: z.string().min(8)
+    })
+
+    const {
+        name, 
+        email,
+        password
+    } = createBodyRequestSchema.parse(request.body)
+
+    
+})
