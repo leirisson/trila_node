bc0d61927d749c400f817624d22dbe739acffd6b
leirisson <leirissonsouza99@gmail.com>
2025-03-05 11:00:42 -0400

    deletei uma pasta que não usava

--- 011-modulo3/06-fundamentos-do-prims-ORM/4-casos-de-uso-e-desing-partterns/4.3-separando-caso-de-uso/http/routes.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { register } from "./controller/registerController";
-
-
-export function appRoutes(app: FastifyInstance){
-    app.post('/users', register)
-}
--- 011-modulo3/06-fundamentos-do-prims-ORM/4-casos-de-uso-e-desing-partterns/4.3-separando-caso-de-uso/src/app.ts
+++
@@ -1,9 +0,0 @@
-import fastify from 'fastify'
-import { appRoutes } from '../http/routes'
-
-
-
-export const app = fastify()
-
-app.register(appRoutes)
-
