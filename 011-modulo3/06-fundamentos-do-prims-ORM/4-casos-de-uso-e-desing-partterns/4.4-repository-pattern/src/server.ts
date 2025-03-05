a07bc66f7148fa3919afa45e1b10bc41b9b3e7d8
leirisson <leirissonsouza99@gmail.com>
2025-03-05 10:58:35 -0400

    criando o projeto no prisma

---
+++ 011-modulo3/06-fundamentos-do-prims-ORM/4-casos-de-uso-e-desing-partterns/4.4-repository-pattern/src/server.ts
@@ -0,0 +1,13 @@
+
+
+import { env } from '../env'
+import {app} from './app'
+
+
+app.listen({
+    host: '0.0.0.0',
+    port: env.PORT
+})
+.then(() => {
+    console.log('Esta rodando o servidor...')
+})
